74ce5b31bf3645c84107c4405a3e0291db5fb269
jayminor <jaydevtechie-stack@users.noreply.github.com>
2026-08-10 17:21:34 +0200

    feat(taskfusion): i18n-ify Dashboard

--- applications/taskfusion/src/i18n/index.js
+++ applications/taskfusion/src/i18n/index.js
@@ -10,6 +10,7 @@ import { initReactI18next } from 'react-i18next';
 import enCommon from './locales/en/common.json';
 import enLanding from './locales/en/landing.json';
 import enNavbar from './locales/en/navbar.json';
+import enDashboard from './locales/en/dashboard.json';
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -17,12 +18,13 @@ i18n.use(initReactI18next).init({
       common: enCommon,
       landing: enLanding,
       navbar: enNavbar,
+      dashboard: enDashboard,
     },
   },
   lng: 'en',
   fallbackLng: 'en',
   defaultNS: 'common',
-  ns: ['common', 'landing', 'navbar'],
+  ns: ['common', 'landing', 'navbar', 'dashboard'],
   interpolation: {
     escapeValue: false, // React already escapes output
   },
